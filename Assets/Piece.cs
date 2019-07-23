d74115b4dc6ecb1868f7472de7621b9f6d3278ba
UHV-VR <gamedevuhv@gmail.com>
2019-07-23 16:47:07 -0500

    making progress

--- Assets/Piece.cs
+++ Assets/Piece.cs
@@ -40,26 +40,20 @@ public class Piece : MonoBehaviour
         {
             //Debug.Log(colInfo.collider.name);
             //  Debug.Log(hitPos.normal);
+
+            //sees if the collider is an interactable piece
             if (collisionInfo.gameObject.GetComponent<Piece>())
             { 
+                //if the collision is happening on the bottom
                 if (hitPos.normal.y < 0 && (collisionInfo.gameObject.GetComponent<Piece>().tagPosition > tagPosition))
                 {
                     gameObject.tag = "bases";
-
-                    /*  Renderer rend = gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>();
-
-                      //Set the main Color of the Material to green
-                      rend.material.shader = Shader.Find("_Color");
-                      rend.material.SetColor("_Color", Color.grey);
-                      //Find the Specular shader and change its Color to red
-                      rend.material.shader = Shader.Find("Specular");
-                      rend.material.SetColor("_SpecColor", Color.grey);
-          */
                     gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.grey);
                 }
             }
 
         }
+        //sees if it is colliding with something its not supose to 
         if (collisionInfo.collider.tag == "noninteractable")
         {
             // and it isn't being held by the remote 
@@ -70,31 +64,42 @@ public class Piece : MonoBehaviour
 
             }
            //  Debug.Log(" transform to new position");
-            //reassigns it to tower 
-            Reset();
+            //reassigns it to the last known tower 
+            move(tower);
         }
     }
 
     void OnCollisionExit(Collision other)
     {
+        //sees if the collider leaving is a piece 
         if (other.gameObject.GetComponent<Piece>())
         {
+            // sees if it is a piece that is smaller 
             if (other.gameObject.GetComponent<Piece>().tagPosition > tagPosition)
             {
                 gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.SetColor("_Color", offColor);
                 gameObject.tag = "interactable";
-                Debug.Log("im leaving and this is why");
-                Debug.Log(other.transform.name);
+               // Debug.Log("im leaving and this is why");
+              //  Debug.Log(other.transform.name);
             }
         }
     }
 
-    void Reset()
+    public void move(Vector3 pos)
     {
-        gameObject.transform.position = tower;
+        gameObject.transform.position = pos;
         gameObject.transform.localEulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
         gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
     }
-    
+    public void setTower(Vector3 pos)
+    {
+        tower = pos;
+    }
+    public void reset()
+    {
+        move(tower);
+    }
+
 }
--- Assets/tower.cs
+++ Assets/tower.cs
@@ -20,6 +20,15 @@ public class tower : MonoBehaviour
     // [SerializeField]
     public GameObject rleft;
 
+    private bool inrange = false;
+    private string inrangename = "null";
+
+    private Vector3 tip;
+    //can get rid of???
+
+    //keeps the order of the pieces the tower interacts with 
+    private int[] order = new int[] { 5, 5, 5, 5 };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +38,7 @@ public class tower : MonoBehaviour
 
         opaqueColor = seethroughColor;
         opaqueColor.a = 1;
+        tip = gameObject.transform.GetChild(0).transform.position;
     }
 
     // Update is called once per frame
@@ -38,11 +48,24 @@ public class tower : MonoBehaviour
     }
     void OnTriggerStay(Collider other)
     {
-        if ((!rright.GetComponent<Hand>().triggerDown || !rleft.GetComponent<Hand>().triggerDown) && (rright.GetComponent<Hand>().held == other.name || rleft.GetComponent<Hand>().held == other.name))
+        if ((rright.GetComponent<Hand>().triggerDown || rleft.GetComponent<Hand>().triggerDown) && (rright.GetComponent<Hand>().held == other.name || rleft.GetComponent<Hand>().held == other.name))
         {
-             Debug.Log("inside getting ready to change color");
+            // Debug.Log("inside getting ready to change color");
             GetComponent<Renderer>().material.SetColor("_Color", seethroughColor);
+            inrange = true;
+            inrangename = other.name;
 
+        }
+        if ((!rright.GetComponent<Hand>().triggerDown && !rleft.GetComponent<Hand>().triggerDown) )
+        {
+           
+            if (inrange && other.name == inrangename)
+            {
+                other.GetComponent<Piece>().move(tip);
+                other.GetComponent<Piece>().setTower(tip);
+                GetComponent<Renderer>().material.SetColor("_Color", opaqueColor);
+                inrange = false; 
+            }
         }
     }
 
@@ -51,7 +74,26 @@ public class tower : MonoBehaviour
         if (other.tag == "interactable")
         {
             GetComponent<Renderer>().material.SetColor("_Color", opaqueColor);
+            inrange = false; 
         }
 
+    }
+
+    //DONT DO THIS HERE! DO THIS IN TRIGGER STAY WITH  if (inrange && other.name == inrangename)
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "interactable")
+        {
+            for (int i = 0; i < order.Length; i++)
+            {
+                if (order[i] == 5)
+                {
+                    if (order[i - 1] < other.collider.GetComponent<Piece>().tagPosition)
+                    {
+                        other.collider.GetComponent<Piece>().reset();
+                    }
+                }
+            }
+        }
     }
 }
