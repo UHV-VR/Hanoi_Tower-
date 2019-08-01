76c266e3960e634cf74b55042a02d832dcb57e8f
UHV-VR <gamedevuhv@gmail.com>
2019-08-01 15:59:13 -0500

    audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public sound[] sounds;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach (sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch; 
+        }
+    }
+
+    public void play(string name)
+    {
+        sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("sound: " + name + " is not found ");
+            return;
+        }
+        s.source.Play();
+    }
+}
